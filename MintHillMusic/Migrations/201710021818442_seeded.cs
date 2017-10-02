10618557bc9a9f3fc878db06f7ab177c1258af08
John Potts <john@arlingtonchurch.com>
2017-10-02 16:11:59 -0400

    add seed data

---
+++ MintHillMusic/Migrations/201710021818442_seeded.cs
@@ -0,0 +1,16 @@
+namespace MintHillMusic.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class seeded : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- MintHillMusic/Migrations/Configuration.cs
+++ MintHillMusic/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace MintHillMusic.Migrations
 {
+    using MintHillMusic.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -15,6 +16,20 @@ namespace MintHillMusic.Migrations
 
         protected override void Seed(MintHillMusic.Models.MintHillMusicContext context)
         {
+
+            context.Songs.AddOrUpdate(
+                x => x.SongID,
+                new Song() { SongID = 1, Name = "She'll be coming round the mountain", Genre = "Folk", Difficulty = "Beginner", ChordChart = "https://www.sheetmusic.com" },
+                new Song() { SongID = 1, Name = "Jungle", Genre = "Rock", Difficulty = "Moderate", ChordChart = "https://www.sheetmusic.com" },
+                new Song() { SongID = 1, Name = "Moonlight sonata", Genre = "Classical", Difficulty = "Advanced", ChordChart = "https://www.sheetmusic.com" },
+                new Song() { SongID = 1, Name = "Call Me Maybe", Genre = "Pop", Difficulty = "Beginner", ChordChart = "https://www.sheetmusic.com" },
+                new Song() { SongID = 1, Name = "Uncle Pen", Genre = "Folk", Difficulty = "Moderate", ChordChart = "https://www.sheetmusic.com" },
+                new Song() { SongID = 1, Name = "Roar", Genre = "Pop", Difficulty = "Beginner", ChordChart = "https://www.sheetmusic.com" }
+                                );
+            
+            
+            
+            
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
